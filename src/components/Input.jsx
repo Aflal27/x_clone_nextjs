d2bc5ea39548940918785efaf5cf6da0f6b21635
Aflal <mohamedaflal6719@gmail.com>
2024-09-15 16:26:23 +0530

    add firebase upload functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,63 @@
 "use client";
-import React from "react";
+
+import React, { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import { HiOutlinePhotograph } from "react-icons/hi";
+import { app } from "../firebase";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 export default function Input() {
   const { data: session } = useSession();
+  const imageRef = useRef(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null); // [1
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (selectedFile) {
+      uploadImageToStorage();
+    }
+  }, [selectedFile]);
+
+  const uploadImageToStorage = () => {
+    setImageFileUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "-" + selectedFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        console.log(error);
+        setImageFileUploading(false);
+        setImageFileUrl(null);
+        setSelectedFile(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
   if (!session) return null;
   return (
     <div className=" p-2 flex border-b border-gray-200 space-x-3 w-full">
@@ -18,8 +71,27 @@ export default function Input() {
           className=" w-full border-none outline-none tracking-wide text-gray-700 min-h-[50px]"
           placeholder="whats happaning"
           rows="2"></textarea>
+        {selectedFile && (
+          <div className="">
+            <img
+              src={imageFileUrl}
+              alt="image"
+              className=" w-full max-h-[250px] object-cover cursor-pointer"
+            />
+          </div>
+        )}
         <div className=" flex items-center justify-between pt-2.5">
-          <HiOutlinePhotograph className=" h-10 w-10 p-2 text-sky-500 hover:text-sky-100 cursor-pointer" />
+          <HiOutlinePhotograph
+            onClick={() => imageRef.current.click()}
+            className=" h-10 w-10 p-2 text-sky-500 hover:text-sky-100 cursor-pointer"
+          />
+          <input
+            hidden
+            onChange={handleImage}
+            accept="image/*"
+            type="file"
+            ref={imageRef}
+          />
           <button className="  bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
             Post
           </button>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpuJiRDeKKOYvnBGrQbw3qODFH68IhBmg",
+  authDomain: "x-clone-fc06e.firebaseapp.com",
+  projectId: "x-clone-fc06e",
+  storageBucket: "x-clone-fc06e.appspot.com",
+  messagingSenderId: "415553846378",
+  appId: "1:415553846378:web:0894c1038595ea55c19e13"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
